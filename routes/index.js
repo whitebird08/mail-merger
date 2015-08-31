0ff46ec8d10b4d41eb1b2e4353f0a56831f5fc1c
Ryan Bird <ryan01bird@gmail.com>
2015-08-30 23:26:01 -0600

    still exploring

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../node_modules/express/lib/data.js') 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(express)
+  var contacts = req.body.contacts
+  var subject = req.body.subject
+  var message = req.body.message
+  var recipients = {preview : data}
+  res.render('index', {preview: data});
 });
-
 
 
 module.exports = router;
