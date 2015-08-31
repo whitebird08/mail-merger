c05e4caf2c87b8405ff550a6bfe612b7a6e0c497
Ryan Bird <ryan01bird@gmail.com>
2015-08-31 00:50:59 -0600

    adjusted form layout

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var data = require('../node_modules/express/lib/data.js') 
-
+var contacts;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(express)
+  res.render('index', { title: 'Mail Merge' });
+});
+router.post('/', function(req, res, next) {
   var contacts = req.body.contacts
   var subject = req.body.subject
   var message = req.body.message
   var recipients = {preview : data}
   res.render('index', {preview: data});
 });
-
 
 module.exports = router;
