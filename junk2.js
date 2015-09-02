bdc31bae4c6ea951e75ce699123633ba847177c6
Ryan Bird <ryan01bird@gmail.com>
2015-09-02 12:45:39 -0600

    recipients is showing up

---
+++ junk2.js
@@ -0,0 +1,12 @@
+////test
+// var form = document.getElementById('form');
+
+// form.addEventListener('submit', function(event) {
+//   var contact = this.elements[0].value
+//   var subject = this.elements[1].value
+//   var message = this.elements[2].value
+//   var preview ='To:' + contact +'Subject' + subject + 'Message:' + message;
+//   document.getElementById('preview').innerHTML = total;
+//   event.preventDefault();
+// })
+/////test
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../node_modules/express/lib/data.js') 
-var contacts;
-/* GET home page. */
+// var data = require('../node_modules/express/lib/data.js') 
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mail Merge' });
 });
 router.post('/', function(req, res, next) {
+  //conditional here? updatePage=true
+  // var updatePage ??????
   var contacts = req.body.contacts
+  console.log(contacts)
   var subject = req.body.subject
+  console.log(subject)
   var message = req.body.message
-  var recipients = {preview : data}
-  res.render('index', {preview: data});
+  console.log(message)
+  
+  var recipient = {preview : message}
+
+  res.render('index', {preview: message});
 });
 
 module.exports = router;
+ 
