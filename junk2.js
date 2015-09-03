af62ce307ea8f6e7052d83937cf19dbcc7961647
Ryan Bird <ryan01bird@gmail.com>
2015-09-02 22:09:54 -0600

    moved split & replace to data.js

--- junk2.js
+++ junk2.js
@@ -10,3 +10,40 @@
 //   event.preventDefault();
 // })
 /////test
+
+function changeText(){
+  var oldHTML = document.getElementById('para').innerHTML;
+  var newHTML = "<span style='color:#ffffff'>" + oldHTML + "</span>";
+  document.getElementById('para').innerHTML = newHTML;
+}
+
+// var contacts = req.body.contacts.split('\r\n')
+  // console.log(contacts)
+  // var contactObjects = [];
+
+  // contacts.forEach(function(contactString){
+  //   var contactArray = contactString.split(',')
+  //   contactObjects.push({
+  //     firstName: contactArray[0], 
+  //     lastName: contactArray[1], 
+  //     email: contactArray[2]
+  //   });
+  // });
+  
+  // var subject = req.body.subject
+  // console.log(subject)
+  // var message = req.body.message
+  // console.log(message)
+
+  // console.log(contactObjects)
+  // var newMessage = message
+  // console.log(message);
+  // console.log(newMessage);
+  // newMessage = newMessage.replace('{first}', contactObjects.firstName);
+  // return newMessage;
+
+  // for each contact in the contact Objects array
+  
+  // replace the {} in the message, with the corresponding property from the contact object
+  // (replace {first}) with obj.firstName
+  // output what we send to the view, should be an array of messages
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,27 @@
 var express = require('express');
 var router = express.Router();
-// var data = require('../node_modules/express/lib/data.js') 
+var data = require('../node_modules/express/lib/data.js') 
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mail Merge' });
 });
 router.post('/', function(req, res, next) {
-  //conditional here? updatePage=true
-  // var updatePage ??????
-  var contacts = req.body.contacts
-  console.log(contacts)
-  var subject = req.body.subject
-  console.log(subject)
-  var message = req.body.message
-  console.log(message)
+ // var updatePage = false;
+  var contacts = req.body.contacts//getting input text from user/client
+  var subject = req.body.subject//getting input text from user/client
+  var message = req.body.message//getting input text from user/client
+  
+  
+  var previewMessage = mailMerge.modifyText(contacts, subject, message)
+
 
   res.render('index', {
     contacts: contacts,
     subject: subject,
-    message: message
+    message: message,
+    previewMessage: previewMessage
   });
+  
 });
 
 module.exports = router;
