ccce84362e2d61b8746843d4411fcab009e5c028
Ryan Bird <ryan01bird@gmail.com>
2015-09-12 19:21:48 -0600

    add comments for delete button and fieldset

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mailMerge = require('../lib/mail_merge') 
-
+  
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mail Merge' });
 });
@@ -11,9 +11,20 @@ router.post('/', function(req, res, next) {
   var contacts = req.body.contacts//getting input text from user/client
   var subject = req.body.subject//getting input text from user/client
   var message = req.body.message//getting input text from user/client
-  
-  var previewMessage = mailMerge(contacts, message);
 
+  var previewMessage = mailMerge(contacts, message);
+  
+/////jquery start
+//   $(".deleteMe").on("click", function(){
+//    $(this).closest("p").remove(); 
+// });
+// /////jquery end
+
+//try this for delete
+// var deletePreviewMessage = function deletePreviewMessage(){
+//   message.remove()
+//////
+}
 
   res.render('index', {
     contacts: contacts,
@@ -24,4 +35,3 @@ router.post('/', function(req, res, next) {
 });
 
 module.exports = router;
- 
